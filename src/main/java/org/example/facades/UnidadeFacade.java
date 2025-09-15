d478ce903a6d5d44931f6455465d8d5ac81b38c9
Jonathas-Ray <jonathasrmcguimaraes@gmail.com>
2025-09-15 00:34:01 -0300

    Partes Unidade e Usuario, precisam de ajustes na lógica de negócio que observarei em aula na próxima quinta feira

---
+++ src/main/java/org/example/facades/UnidadeFacade.java
@@ -0,0 +1,34 @@
+package org.example.facades;
+
+import org.example.applications.UnidadeApplication;
+import org.example.entities.Unidade;
+
+import java.util.List;
+
+public class UnidadeFacade {
+    private final UnidadeApplication unidadeApplication;
+
+    public UnidadeFacade(UnidadeApplication unidadeApplication) {
+        this.unidadeApplication = unidadeApplication;
+    }
+
+    public List<Unidade> buscarTodos() {
+        return this.unidadeApplication.buscarTodos();
+    }
+
+    public Unidade buscarPorId(int id) {
+        return this.unidadeApplication.buscarPorId(id);
+    }
+
+    public void adicionar(Unidade unidade) {
+        this.unidadeApplication.adicionar(unidade);
+    }
+
+    public void excluir(int id) {
+        this.unidadeApplication.excluir(id);
+    }
+
+    public void atualizar(int id, Unidade unidade) {
+        this.unidadeApplication.atualizar(id, unidade);
+    }
+}
