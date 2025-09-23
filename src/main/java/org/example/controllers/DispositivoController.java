119aac6ada71da8d75ad69ee1852fa03b4b91e73
Denvx <denversonalii@gmail.com>
2025-09-23 13:49:55 -0300

    Atualizações do models e controllers

---
+++ src/main/java/org/example/controllers/DispositivoController.java
@@ -0,0 +1,46 @@
+package org.example.controllers;
+
+import org.example.entities.Dispositivo;
+import org.example.facades.DispositivoFacade;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/Dispositivo")
+public class DispositivoController{
+
+    private final DispositivoFacade dispositivoFacade;
+
+    public DispositivoController(DispositivoFacade dispositivoFacade){
+        this.dispositivoFacade = dispositivoFacade;
+    }
+
+    @GetMapping
+    public List<Dispositivo> getDispositivo(){
+        return dispositivoFacade.buscarTodos();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Dispositivo> GetDispositivo(@PathVariable int id){
+        Dispositivo dispositivo = dispositivoFacade.buscarPorId(id);
+        if (dispositivo != null){
+            return ResponseEntity.ok(dispositivo);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public void criarDispositivo(@RequestBody Dispositivo dispositivo){
+        dispositivoFacade.adicionar(dispositivo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removerDispoitivo(@PathVariable int id){
+        dispositivoFacade.excluir(id);
+    }
+}
+
