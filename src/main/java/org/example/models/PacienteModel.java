2f09a2388be39a4d0317b6af058bfa2d8ab16b76
NatanAmigo <153786713+NatanAmigo@users.noreply.github.com>
2025-09-22 22:52:01 -0300

    PacienteModel e SensorModel

---
+++ src/main/java/org/example/models/PacienteModel.java
@@ -0,0 +1,37 @@
+package org.example.models;
+
+public class PacienteModel {
+    private int id;
+    private String nome;
+    private String referencia;
+
+    public PacienteModel(int id, String nome, String referencia) {
+        this.id = id;
+        this.nome = nome;
+        this.referencia = referencia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+}
