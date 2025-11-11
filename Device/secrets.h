5e8ea64ad65d785b309344e94dcea82ac7df2f5a
Jonathas-Ray <jonathasrmcguimaraes@gmail.com>
2025-11-11 19:38:20 -0300

    versão em produção do código IoT, falta reparar a conexão com Firebase

---
+++ Device/secrets.h
@@ -0,0 +1,26 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// Array de redes WiFi (4 redes)
+const char* WIFI_SSIDS[] = {
+    "Hhsovov-#+$(hei",
+    "Maria Isaltina",
+    "SENAI_ACADEMICO", 
+    "Vidal_Cliente"
+};
+
+const char* WIFI_PASSWORDS[] = {
+    "12343212",
+    "39783978",
+    "Senai*Academico",
+    "clientevidal"
+};
+
+#define NUM_REDES 4  // Número total de redes
+
+#define FIREBASE_HOST       "vigiamed-default-rtdb.firebaseio.com"
+#define FIREBASE_API_KEY    "AIzaSyDfAb5ixYgDdOgGO8THkwV_kpLWVCOVyR0"
+#define FIREBASE_EMAIL      "pulsanapulsana@gmail.com"
+#define FIREBASE_PASSWORD   "d9`4w8E8@2Qb"
+
+#endif
