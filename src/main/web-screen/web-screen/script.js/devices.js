2bf8f1b66b5f30e8f4d86bdb9e7b9492d313905a
Jonathas-Ray <jonathasrmcguimaraes@gmail.com>
2025-11-26 20:03:48 -0300

    esqueca

--- src/main/web-screen/web-screen/script.js/devices.js
+++
@@ -1,4 +0,0 @@
-import { auth, db, rtdb } from '../firebase-config.js';
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
-import { ref, onValue } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
