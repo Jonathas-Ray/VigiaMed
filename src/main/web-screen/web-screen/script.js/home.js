d8340dab0cb8c9db2bc3341b028cd4f26d9f051b
Denvx <denversonalii@gmail.com>
2025-12-08 20:15:25 -0300

    tudo oskay

--- src/main/web-screen/web-screen/script.js/home.js
+++ src/main/web-screen/web-screen/script.js/home.js
@@ -14,28 +14,22 @@ const saturationEl = document.getElementById('saturation');
 const temperatureEl = document.getElementById('temperature');
 const deviceIdEl = document.getElementById('deviceID');
 
-<<<<<<< HEAD
 const DEVICE_ID = "DCB4D905BF3C"; 
 const API_BASE_URL = "http://localhost:8080";
 
 let ultimoAlertaMostrado = null;
 let ultimoResultado = null;
-=======
 
-const DEVICE_ID = "DCB4D905BF3C";
 const API_CALL_INTERVAL_MS = 10 * 1000;
 
 
 function startApiScheduler(deviceId) {
     
-    // Função que será executada a cada 10 segundos
     const sendData = () => {
-        // 1. LÊ OS VALORES ATUAIS do display (eles foram atualizados pelo onValue)
         const bpm = heartRateEl.textContent;
         const spo2 = saturationEl.textContent;
         const temp = temperatureEl.textContent;
 
-        // 2. Checa se há dados válidos
         if (bpm !== '--' && spo2 !== '--' && temp !== '--') {
             
             const dadosParaAPI = {
@@ -46,32 +40,27 @@ function startApiScheduler(deviceId) {
             
             const nextSendTime = API_CALL_INTERVAL_MS / 1000;
 
-            console.log(`⏱️ Enviando dados para API. Próximo envio em ${nextSendTime}s.`);
+            console.log(`Enviando dados para API. Próximo envio em ${nextSendTime}s.`);
 
             enviarMedicaoCompleta(deviceId, dadosParaAPI)
                 .catch(err => console.error("Falha ao salvar medição via API:", err));
         } else {
-            console.warn("⚠️ Dados incompletos ou inválidos. Envio para API ignorado.");
+            console.warn("Dados incompletos ou inválidos. Envio para API ignorado.");
         }
     };
     
-    // Inicia a execução imediatamente e repete a cada 10 segundos
-    sendData(); // Roda a primeira vez imediatamente
+    sendData(); 
     setInterval(sendData, API_CALL_INTERVAL_MS);
 }
->>>>>>> feature-heitor
 
 document.addEventListener('DOMContentLoaded', () => {
     onAuthStateChanged(auth, (user) => {
         if (user) {
             fetchUserInfo(user.uid);
             listenToVitals(DEVICE_ID);
-<<<<<<< HEAD
             iniciarMonitoramento(); 
-=======
 
             startApiScheduler(DEVICE_ID); 
->>>>>>> feature-heitor
         } else {
             window.location.href = '../html/login.html';
         }
@@ -119,7 +108,6 @@ function listenToVitals(deviceId) {
             const spo2 = data.spo2?.value || '--';
             const temp = data.Temperature?.value || '--'; 
             
-            // Apenas atualiza o display com os dados mais recentes
             updateValue(heartRateEl, bpm);
             updateValue(saturationEl, spo2);
             updateValue(temperatureEl, temp);
