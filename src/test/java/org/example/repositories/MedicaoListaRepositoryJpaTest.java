7abf3879ae7fa706bc182e4ae081a81ffae3a251
NatanAmigo <natan.souza2711@gmail.com>
2026-03-25 20:43:37 -0300

    Ajeitando 2x

--- src/test/java/org/example/repositories/MedicaoListaRepositoryJpaTest.java
+++ src/test/java/org/example/repositories/MedicaoListaRepositoryJpaTest.java
@@ -3,7 +3,10 @@ package org.example.repositories;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.example.interfaces.MedicaoListaModelRepositoryJpa;
+import org.example.interfaces.MedicaoModelRepositoryJpa;
 import org.example.models.MedicaoListaModel;
+import org.example.models.MedicaoModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
